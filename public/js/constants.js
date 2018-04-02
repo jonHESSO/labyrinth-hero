024340eff56aa3ab87cddb9b10d55443b1012e57
fterrani <fabien.terrani@students.hevs.ch>
2018-04-02 14:17:06 +0200

    js folder created, JS constants file added

---
+++ public/js/constants.js
@@ -0,0 +1,17 @@
+var Tile = {
+	BOUNDARY: -1,
+	GROUND: 0,
+	WALL: 1,
+	UNDESTRUCTIBLE_WALL: 2,
+	ENTRANCE: 3,
+	EXIT: 4
+};
+
+var Item = {
+	VOID: 0,
+	PICKAXE: 1,
+	PATH_HIGHLIGHT: 2,
+	BOMB: 3,
+	TELEPORTER: 4,
+	COIN: 5
+};
